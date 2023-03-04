470439aa885cee0f6959fea51e86a156f50cde03
333Nikita333 <nikitosfox333@gmail.com>
2023-03-04 22:35:18 +0200

    inserted the old code with classes and refactored App component on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import { useState } from 'react';
+import { FeedbackOptions } from 'components/Feedback';
+import { Statistics } from 'components/Statistics';
+import { Section } from 'components/Section';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = ['Good', 'Neutral', 'Bad'];
+  const sumFeedback = good + neutral + bad;
+
+  function onLeaveFeedback(e) {
+    switch (e.target.textContent) {
+      case 'Good':
+        setGood(prevValue => prevValue + 1);
+        break;
+      case 'Neutral':
+        setNeutral(prevValue => prevValue + 1);
+        break;
+      case 'Bad':
+        setBad(prevValue => prevValue + 1);
+        break;
+      default:
+        return;
+    }
+  }
+
+  function countPositiveFeedbackPercentage() {
+    if (sumFeedback === 0) return 0;
+
+    const totalFeedback = (good / sumFeedback) * 100;
+    return Math.round(totalFeedback);
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section title="Please leave feedback">
+      <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={sumFeedback}
+        positivePercentage={countPositiveFeedbackPercentage()}
+      />
+    </Section>
   );
-};
+}
